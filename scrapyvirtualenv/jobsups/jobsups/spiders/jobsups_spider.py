6ff31cc5dc6af6f37b0590eab8f10d6e9e978c0f
mezcel <mezcel@mail.com>
2017-10-03 16:16:13 -0400

    ups jobs update

--- scrapyvirtualenv/jobsups/jobsups/spiders/jobsups_spider.py
+++ scrapyvirtualenv/jobsups/jobsups/spiders/jobsups_spider.py
@@ -1,22 +1,28 @@
 import scrapy
+import datetime
 
-class JobUpsSpider(scrapy.Spider):
-    name = 'jobfairsin'
+class JobsUpsSpider(scrapy.Spider):
+    name = 'jobsups'
     allowed_domain = ["hhttps://www.jobs-ups.com/"]
     #start_urls = [ 'https://www.jobs-ups.com/search-jobs/Florida' ]
 
     def __init__(self, domain='', *args,**kwargs):
-        super(JobUpsSpider, self).__init__(*args, **kwargs)
+        super(JobsUpsSpider, self).__init__(*args, **kwargs)
         self.start_urls = [domain]
 
 
     def parse(self, response):
         # scrape contact info
         for result in response.xpath('//*[@id="search-results-list"]/ul/li'):
-            tempAddrArr = [i.strip() for i in result.xpath('a/span/text()').extract()]
+            # tempAddrArr = [i.strip() for i in result.xpath('a/span/text()').extract()]
+            # tempAddrArr = ", ".join(tempAddrArr)
+            tempAddrArr = result.xpath('a/span[2]/text()').extract()
+            tempAddrArr = tempAddrArr + result.xpath('a/span[3]/text()').extract()
             tempAddrArr = ", ".join(tempAddrArr)
             yield {
                 'url': "https://www.jobs-ups.com" + result.xpath('a/@href').extract_first(),
                 'title': result.xpath('a/h2/text()').extract_first(),
-                'address': tempAddrArr,
+                'address': "UPS, " + tempAddrArr,
+                'company': result.xpath('a/span[1]/text()').extract_first(),
+                'date': datetime.datetime.now().strftime ("%Y%m%d"),
             }
--- scrapyvirtualenv/jobsups/readme.md
+++ scrapyvirtualenv/jobsups/readme.md
@@ -1,6 +1,15 @@
 rm '/home/mezcel/github/scrapy-scrape-viewer/jobsups_Florida_Demo.json'
+rm '/home/mezcel/github/scrapy-scrape-viewer/jobsups_Georgia_Demo.json'
+rm '/home/mezcel/github/scrapy-scrape-viewer/jobsups_Alabama_Demo.json'
+
 cd '/home/mezcel/github/scrapy-scrape-viewer/scrapyvirtualenv'
+
 source bin/activate
+
 ls
+
 cd jobsups
+
 scrapy crawl jobsups -a domain='https://www.jobs-ups.com/search-jobs/Florida' -o ../../jobsups_Florida_Demo.json -t json
+scrapy crawl jobsups -a domain='https://www.jobs-ups.com/search-jobs/Georgia' -o ../../jobsups_Georgia_Demo.json -t json
+scrapy crawl jobsups -a domain='https://www.jobs-ups.com/search-jobs/Alabama' -o ../../jobsups_Alabama_Demo.json -t json
