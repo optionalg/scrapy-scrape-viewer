75605691847f0d04c830c55cce75229c47bd07d0
mezcel <mezcel@mail.com>
2017-10-09 04:56:39 -0400

    messed with shell script

--- scrapy-scrape-viewer.sh
+++ scrapy-scrape-viewer.sh
@@ -3,6 +3,7 @@ source /home/mezcel/github/scrapy-scrape-viewer/scrapyvirtualenv/expresspros/exp
 source /home/mezcel/github/scrapy-scrape-viewer/scrapyvirtualenv/jobsups/upsjobsDemoShell.sh
 source /home/mezcel/github/scrapy-scrape-viewer/scrapyvirtualenv/stackoverflow/stackoverflowDemoShell.sh
 source /home/mezcel/github/scrapy-scrape-viewer/scrapyvirtualenv/targetedjobfairs/targetedjobfairsDemoShell.sh
-run in firefox
-firefox /home/mezcel/github/scrapy-scrape-viewer/index.html
+# run in firefox
+# open all 3 relevant apps
+firefox -new-tab -url /home/mezcel/github/scrapy-scrape-viewer/index.html -new-tab -url /home/mezcel/github/googlemaps-api-helloworld/index.html -new-tab -url /home/mezcel/github/document-generator/index.html &
 exit
